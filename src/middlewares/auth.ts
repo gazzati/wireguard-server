37cad60530e6ca32654445c066968eb486324518
tgazzaev <tgazzaev@samokat.ru>
2024-09-14 22:53:51 +0300

    add auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -6,7 +6,7 @@ import { config } from "../config"
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    if (!config.ipWhitelist.includes(req.ip) && req.params.api_token !== config.apiToken) {
+    if (!config.ipWhitelist.includes(req.ip) && req.query.api_token !== config.apiToken) {
       return res.status(401).send({ success: false, message: "Unauthorized" })
     }
 
