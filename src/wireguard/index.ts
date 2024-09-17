1e716cfc3d85215441bd3c8ac041fd5082c562bf
tgazzaev <tgazzaev@samokat.ru>
2024-09-17 12:05:22 +0300

    switch to syncconf

--- src/wireguard/index.ts
+++ src/wireguard/index.ts
@@ -147,10 +147,10 @@ class Wireguard {
   }
 
   private async restartWg() {
-    // await exec(`wg syncconf ${wgParams.SERVER_WG_NIC} <(wg-quick strip ${wgParams.SERVER_WG_NIC})`)
+    await this.exec(`wg syncconf ${wgParams.SERVER_WG_NIC} <(wg-quick strip ${wgParams.SERVER_WG_NIC})`)
 
-    await this.exec(`wg-quick down ${wgParams.SERVER_WG_NIC}`)
-    await this.exec(`wg-quick up ${wgParams.SERVER_WG_NIC}`)
+    //await this.exec(`wg-quick down ${wgParams.SERVER_WG_NIC}`)
+    //await this.exec(`wg-quick up ${wgParams.SERVER_WG_NIC}`)
   }
 
   private async exec(command: string) {
